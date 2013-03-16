1da97bdbef0b52c3ce272317d2beaba6c8d6d343
cravip <cravip1512@gmail.com>
2013-03-16 23:43:47 +0530

    Update build.xml

--- src/wordbuilder/WordBuilder.java
+++ src/wordbuilder/WordBuilder.java
@@ -4,6 +4,12 @@
  */
 package wordbuilder;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.TreeSet;
+
 /**
  *
  * @author Mohit
@@ -13,10 +19,18 @@ public class WordBuilder {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
-         System.setProperty("wordnet.database.dir", "wordnet/dict");
+      //  Properties props = System.getProperties();
+    //    props.setProperty("wordnet.database.dir", "wordnet/dict"); 
+      //  System.setProperty("wordnet.database.dir","../wordnet/dict");
+       System.setProperty("wordnet.database.dir", "C:/Program Files (x86)/WordNet/2.1/dict");
+   //     String a = System.getProperty("user.dir");
+     //   a+="/WordBuilder.jar/wordnet/dict/";
+      //  System.setProperty("wordnet.database.dir", a);
         ApiFetch.main(args);
         //MainPage.main(args);
+     
+       
     }
 }
