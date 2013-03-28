89e417d6d275a3fc7ee667360d01282ec3290018
cravip <cravip1512@gmail.com>
2013-03-29 01:42:57 +0530

    Add Apifetch functions and update wordnet database to 3.1

--- src/wordbuilder/ApiFetch.java
+++ src/wordbuilder/ApiFetch.java
@@ -3,86 +3,251 @@
  * and open the template in the editor.
  */
 package wordbuilder;
+
 import edu.smu.tspell.wordnet.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-
-
-
-
-/**
- * Displays word forms and definitions for synsets containing the word form
- * specified on the command line. To use this application, specify the word
- * form that you wish to view synsets for, as in the following example which
- * displays all synsets containing the word form "airplane":
- * <br>
- * java TestJAWS airplane
- */
 /**
  *
- * @author snooze-wake
+ * @author chandra
  */
 public class ApiFetch {
-    
+
+    public static void main(String[] args) {
+        if (true) {
+            //  Concatenate the command-line arguments
+            StringBuffer buffer = new StringBuffer();
+            /*for (int i = 0; i < args.length; i++)
+             {
+             buffer.append((i > 0 ? " " : "") + args[i]);
+             }*/
+
+            //	String wordForm = buffer.toString();
+            Scanner in = new Scanner(System.in);
+            String wordForm = in.nextLine();
+            WordSense[] test = new WordSense[1];
+            System.out.println(wordForm);
+            //  Get the synsets containing the wORd form
+            database = WordNetDatabase.getFileInstance();
+            Synset[] synsets = database.getSynsets(wordForm, SynsetType.NOUN);
+            System.out.println(synsets);
+
+            //  Display the word forms and definitions for synsets retrieved
+            //    System.out.println(synsets.length);
+            if (synsets.length > 0) {
+                System.out.println("The following synsets contain '"
+                        + wordForm + "' or a possible base form "
+                        + "of that text:");
+                for (int i = 0; i < synsets.length; i++) {
+                    System.out.println("");
+                    System.out.println(synsets[i]);                             // total synsets
+                    //   System.out.println("frequency " + synsets[i].getTagCount(wordForm));
+                    String[] wordForms = synsets[i].getWordForms();             // all wordforms in a particular synset
+                    NounSynset ns1 = (NounSynset) synsets[i];
+                    NounSynset[] ns2 = ns1.getHypernyms();
+                    int f = ns2.length;
+                    while (f > 0) {
+                        f--;
+                        System.out.println("nounsynset are " + ns2[f]);
 
 
-	/**
-	 * Main entry point. The command-line arguments are concatenated together
-	 * (separated by spaces) and used as the word form to look up.
-	 */
-	public static void main (String[] args)
-	{
-		if (true)
-		{
-			//  Concatenate the command-line arguments
-			StringBuffer buffer = new StringBuffer();
-			/*for (int i = 0; i < args.length; i++)
-			{
-				buffer.append((i > 0 ? " " : "") + args[i]);
-			}*/
-                    
-		//	String wordForm = buffer.toString();
-                        Scanner in = new Scanner(System.in);
-                        String wordForm = in.nextLine();
-			WordSense []test = new WordSense[1];
-			System.out.println(wordForm);
-			//  Get the synsets containing the wrod form
-			WordNetDatabase database = WordNetDatabase.getFileInstance();
-			Synset[] synsets = database.getSynsets(wordForm);
-			//System.out.println(database.getTagCount("kick"));
-			//  Display the word forms and definitions for synsets retrieved
-			if (synsets.length > 0)
-			{
-				System.out.println("The following synsets contain '" +
-						wordForm + "' or a possible base form " +
-						"of that text:");
-				for (int i = 0; i < synsets.length; i++)
-				{
-					System.out.println("");
-					String[] wordForms = synsets[i].getWordForms();
-					//System.out.println(synsets[i].getTagCount("kick"));
-					test = synsets[i].getAntonyms("left");
-					//if (test[0] != null)
-					//System.out.println(test[0]);
-					for (int j = 0; j < wordForms.length; j++)
-					{
-					//	System.out.print((j > 0 ? ", " : "") +
-								System.out.print(wordForms[j]);
-					}
-					System.out.println(": " + synsets[i].getDefinition());
-				}
-			}
-			else
-			{
-				System.err.println("No synsets exist that contain " +
-						"the word form '" + wordForm + "'");
-			}
-		}
-		else
-		{
-			System.err.println("You must specify " +
-					"a word form for which to retrieve synsets.");
-		}
-	}
 
+                    }
+
+
+                    //         System.out.println(wordForms.length);
+                    //System.out.println(synsets[i].getTagCount("kick"));
+                    //	test = synsets[i].getAntonyms("left");
+                    //if (test[0] != null)
+                    //System.out.println(test[0]);
+                    for (int j = 0; j < wordForms.length; j++) {
+                        //System.out.print((j > 0 ? ", " : "") +
+                        //	System.out.print(wordForms[j]);
+                    }
+                    //System.out.println(": " + synsets[i].getDefinition());
+                    break;
+                }
+            } else {
+                System.err.println("No synsets exist that contain "
+                        + "the word form '" + wordForm + "'");
+            }
+
+        } else {
+            System.err.println("You must specify "
+                    + "a word form for which to retrieve synsets.");
+        }
+    }
+
+    /**
+     * Initialise instance for wordnet database search.
+     */
+    public static void initInstance() {
+        if (!initiscalled) {
+            database = WordNetDatabase.getFileInstance();
+            initiscalled = true;
+        }
+
+    }
+
+    /**
+     *
+     * @param input Word for which three level Hypernyms are to be obtained
+     * @return Arraylist contains all three level Hypernyms. First entry
+     * contains number of wordforms in Level 1 Hypernym and corresponding
+     * wordsforms are followed, same thing is repeated for Level 2 and Level 3.
+     */
+    public static ArrayList<String> getHypernym(String input) {
+        initInstance();
+        ArrayList<String> al2 = new ArrayList();
+        Synset[] synset = database.getSynsets(input, SynsetType.NOUN);
+        if (synset.length > 0) {
+            NounSynset ns1 = (NounSynset) synset[0];                         // Top most synset in noun synsets.
+            NounSynset[] nsL1 = ns1.getHypernyms();
+            if (nsL1.length > 0) {
+                String[] hypernymwordsL1 = nsL1[0].getWordForms();
+                if (hypernymwordsL1.length > 0) // if there is noun hypernym of the given word present 
+                {
+
+                    al2.add(String.valueOf(hypernymwordsL1.length));
+                    for (int i = 0; i < hypernymwordsL1.length; i++) {
+                        al2.add(hypernymwordsL1[i]);
+                    }
+
+                }
+                NounSynset[] nsL2 = nsL1[0].getHypernyms();
+                if (nsL2.length > 0) {
+                    String[] hypernymwordsL2 = nsL2[0].getWordForms();
+                    if (hypernymwordsL2.length > 0) // if there is noun hypernym of the given word present 
+                    {
+
+                        al2.add(String.valueOf(hypernymwordsL2.length));
+                        al2.addAll(Arrays.asList(hypernymwordsL2));
+
+                    }
+                    NounSynset[] nsL3 = nsL2[0].getHypernyms();
+                    if (nsL3.length > 0) {
+                        String[] hypernymwordsL3 = nsL3[0].getWordForms();
+                        if (hypernymwordsL3.length > 0) // if there is noun hypernym of the given word present 
+                        {
+
+                            al2.add(String.valueOf(hypernymwordsL3.length));
+                            al2.addAll(Arrays.asList(hypernymwordsL3));
+
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+            for (int i=0;i< al2.size();i++)
+        {
+            System.out.println(al2.get(i));
+        }
+        return al2;
+    }
+
+    /**
+     *
+     * @param uid The user who has searched the word
+     * @param word The word to be serached
+     * @param why Purpose of searching the word viz, q or s
+     * @return String Arraylist will contain meaning of the word and all the
+     * wordforms present in the synset.
+     */
+    public static ArrayList<String> getMeaning(String uid, String word, String why) {
+        initInstance();
+        ArrayList<String> al1 = new ArrayList<>();
+        Synset[] synset = database.getSynsets(word);
+        int[] frequency = new int[synset.length];
+        int max = -1;
+        int index = 0;
+        if (synset.length > 0) {
+            for (int i = 0; i < synset.length; i++) {
+
+                frequency[i] = synset[i].getTagCount(word);
+
+            }
+            for (int j = 0; j < synset.length; j++) {
+                if (frequency[j] > max) {
+                    index = j;
+                    max = frequency[j];
+                }
+
+            }
+            al1.add(synset[index].getDefinition());
+            String[] wordForms = synset[index].getWordForms();
+            for (int j = 0; j < wordForms.length; j++) {
+                al1.add(wordForms[j]);
+            }
+
+        }
+
+        return al1;
+
+    }
+
+    /**
+     * 
+     * @param input Word for which three level hyponyms are to be obtained
+     * @return Arraylist contains all three level Hyponyms. First entry
+     *         contains number of wordforms in Level1 Hyponym and corresponding 
+     *         wordforms are followed, same thing is repeated for Level 2 and Level 3
+     */
+    public static ArrayList<String> getHyponym(String input) {
+        initInstance();
+        ArrayList<String> al3 = new ArrayList();
+        Synset[] synset = database.getSynsets(input, SynsetType.NOUN);
+        if (synset.length > 0) {
+            NounSynset ns1 = (NounSynset) synset[0];                         // Top most synset in noun synsets.
+            NounSynset[] nsL1 = ns1.getHyponyms();
+            if (nsL1.length > 0) {
+                String[] hyponymwordsL1 = nsL1[0].getWordForms();
+                if (hyponymwordsL1.length > 0) // if there is noun hyponym of the given word present 
+                {
+
+                    al3.add(String.valueOf(hyponymwordsL1.length));
+                    for (int i = 0; i < hyponymwordsL1.length; i++) {
+                        al3.add(hyponymwordsL1[i]);
+                    }
+
+                }
+                NounSynset[] nsL2 = nsL1[0].getHyponyms();
+                if (nsL2.length > 0) {
+                    String[] hyponymwordsL2 = nsL2[0].getWordForms();
+                    if (hyponymwordsL2.length > 0) // if there is noun hyponym of the given word present 
+                    {
+
+                        al3.add(String.valueOf(hyponymwordsL2.length));
+                        al3.addAll(Arrays.asList(hyponymwordsL2));
+
+                    }
+                    NounSynset[] nsL3 = nsL2[0].getHyponyms();
+                    if (nsL3.length > 0) {
+                        String[] hyponymwordsL3 = nsL3[0].getWordForms();
+                        if (hyponymwordsL3.length > 0) // if there is noun hyponym of the given word present 
+                        {
+
+                            al3.add(String.valueOf(hyponymwordsL3.length));
+                            al3.addAll(Arrays.asList(hyponymwordsL3));
+
+                        }
+                    }
+
+                }
+
+            }
+
+        }
+        for (int i=0;i< al3.size();i++)
+        {
+            System.out.println(al3.get(i));
+        }
+        return al3;
+    }
+    private static WordNetDatabase database;
+    private static boolean initiscalled = false;
 }
