de074023da20bcea8f989274d65ce7b753af7fd6
cravip <cravip1512@gmail.com>
2013-04-04 21:15:25 +0530

    Improve Pop up and threading in Background

--- src/wordbuilder/Notify.java
+++ src/wordbuilder/Notify.java
@@ -9,8 +9,10 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Shape;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
+import java.awt.geom.RoundRectangle2D;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -21,6 +23,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.Timer;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
 
 /**
@@ -29,35 +32,41 @@ import javax.swing.WindowConstants;
  */
 public class Notify extends JFrame {
 
-    public static void showNotification(String input) {
+    public static void showNotification(String input) throws ClassNotFoundException {
 
         if (!iscalled && Notify.getNotify()) {
+          
+            
             iscalled = true;
             message = " ";
             header = " ";
             frame = new JFrame();
-            frame.setSize(300, 125);
+            frame.setSize(300, 120);
             frame.setUndecorated(true);
             frame.setLayout(new GridBagLayout());
             // frame.setBackground(Color.yellow);
-
+            Dimension arcSize = new Dimension(50, 50);
+            frame.setShape(new RoundRectangle2D.Double
+            (0d, 0d, frame.getWidth(), frame.getHeight(), arcSize.width, arcSize.height));
+            
+            
             GridBagConstraints constraints = new GridBagConstraints();
             constraints.gridx = 0;
             constraints.gridy = 0;
             constraints.weightx = 1.0f;
             constraints.weighty = 1.0f;
-            constraints.insets = new Insets(5, 5, 5, 5);
+           // constraints.insets = new Insets(5, 5, 5, 5);
             constraints.fill = GridBagConstraints.BOTH;
             headingLabel = new JLabel(header);
 
             //        headingLabel .setIcon(headingIcon); // --- use image icon you want to be as heading image.
             headingLabel.setOpaque(true);
             frame.add(headingLabel, constraints);
-            constraints.gridx++;
+          /*  constraints.gridx++;
             constraints.weightx = 0f;
             constraints.weighty = 0f;
             constraints.fill = GridBagConstraints.NONE;
-            constraints.anchor = GridBagConstraints.NORTH;
+            constraints.anchor = GridBagConstraints.NORTH; */
             /*     JButton cloesButton = new JButton(new AbstractAction("x") {
              @Override
              public void actionPerformed(final ActionEvent e) {
@@ -67,14 +76,15 @@ public class Notify extends JFrame {
              * */
             //  frame.add(cloesButton, constraints);
             constraints.gridx = 0;
-            constraints.gridy++;
+            constraints.gridy = 1;
             constraints.weightx = 1.0f;
             constraints.weighty = 1.0f;
-            constraints.insets = new Insets(5, 5, 5, 5);
-            constraints.fill = GridBagConstraints.BOTH;
+           // constraints.insets = new Insets(5, 5, 5, 5);
+            constraints.fill = GridBagConstraints.BOTH; 
             messageLabel = new JLabel("<HtMl>" + message);
             messageLabel.setOpaque(true);
-            messageLabel.setForeground(Color.red);
+            messageLabel.setBackground(Color.getHSBColor(0, 0, 1.05f));
+            headingLabel.setBackground(Color.getHSBColor(0, 0, 1.05f));
             frame.add(messageLabel, constraints);
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             Dimension scrSize = Toolkit.getDefaultToolkit().getScreenSize();// size of the screen
@@ -97,16 +107,18 @@ public class Notify extends JFrame {
         if (Notify.getNotify()) {
             //  System.out.println("I am here");
 
-            header = "Hi " + WordBuilder.getCurrentUser();
-
+            header ="<html><B>"+"   Hi </B> <I>"  + WordBuilder.getCurrentUser()+ "</I> </html>" ;
+            String empty = "  " ;
             if (!input.equals("")) {
                 message = input;
+                messageLabel.setForeground(Color.WHITE);
             } else {
                 message = "No Such Word Found";
+                messageLabel.setForeground(Color.WHITE);
             }
             headingLabel.setText(header);
             messageLabel.setText("<HTML>" + message);
-
+            headingLabel.setForeground(Color.WHITE);
 
             if (frame.isVisible() == false) {
                 frame.setVisible(true);
@@ -165,7 +177,7 @@ public class Notify extends JFrame {
     static String header;
     static JFrame frame;
     static boolean iscalled = false;
-    private static JLabel messageLabel = new JLabel("<HtMl>" + message);
+    private static JLabel messageLabel ;
     private static JLabel headingLabel;
     private static Timer t1;
     private static boolean isNotify = true;
